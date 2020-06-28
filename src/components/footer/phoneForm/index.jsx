c8fd918279678615b6d2b2cf65b1fc7a82658114
michl1001 <mlshaojr@gmail.com>
2020-06-28 17:42:53 -0400

    Implemented phone input handling

--- src/components/footer/phoneForm/index.jsx
+++ src/components/footer/phoneForm/index.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import NumberFormat from 'react-number-format';
 import './style.css';
 
 
 class PhoneForm extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {number: ''};
-    
+        this.state = {
+            numberString:'',
+            number: 0
+        };
+        
+        this.prevState = '';
+        this.submitMessage = '';
+        this.submitted = false;
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
       }
     
-      handleChange(event) {
-        this.setState({number: event.target.value});
+      handleChange(values) {
+        if(values.value !== this.prevState){
+            this.prevState = this.state.number;
+            this.setState({
+                numberString: values.value,
+                number: values.floatValue,
+                submitted: false
+            });
+        }
       }
     
       handleSubmit(event) {
-        alert('A number was submitted: ' + this.state.value);
+        if( this.state.numberString.length === 10 && !isNaN(this.state.numberString) ){
+            this.prevState = '';
+            this.submitMessage = "Phone Number submitted! Submit another?";
+            alert("success, got: " + this.state.numberString);
+        }
+        else{
+            this.submitMessage = "Please enter a valid number: (Example: (XXX)-XXX-XXXX)"
+        }
+
+        this.setState({
+            numberString:'',
+            number: 0,
+            submitted: true
+        });
         event.preventDefault();
       }
     
@@ -24,10 +51,19 @@ class PhoneForm extends React.Component{
         return (
           <form onSubmit={this.handleSubmit}>
             <label>
-              <div className="prompt">Get SMS alerts for the latest updates:</div>
-              <input className="inputPhone" type="tel" value={this.state.value} onChange={this.handleChange} />
+              <div className="prompt">{this.state.submitted ? this.submitMessage:"Get SMS alerts for the latest updates:"}</div>
+              <NumberFormat 
+                type="tel" 
+                className="inputPhone" 
+                format="+1 (###) ###-####" 
+                allowEmptyFormatting mask=" " 
+
+                getInputRef = {(refer) => this.inputPhone = refer}
+                value={this.state.value} 
+                onValueChange={(values) => {this.handleChange(values)}}
+              />
             </label>
-            <input className="phoneSubmit" type="submit" value="Submit" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" />
+            <input className="phoneSubmit" type="submit" value="Submit" />
           </form>
         );
       }
